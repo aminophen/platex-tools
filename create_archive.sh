9b4f3267936d08b2b19cfd9ef2d1a8fd001e27f0
aminophen <h.y.acetaminophen@gmail.com>
2016-10-12 19:56:53 +0900

    Add create_archive.sh

---
+++ create_archive.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+PROJECT=platex-tools
+TMP=/tmp
+PWDF=`pwd`
+LATESTRELEASEDATE=`git tag | sort -r | head -n 1`
+RELEASEDATE=`git tag --points-at HEAD | sort -r | head -n 1`
+
+if [ -z "$RELEASEDATE" ]; then
+    RELEASEDATE="**not tagged**; later than $LATESTRELEASEDATE?"
+fi
+
+echo " * Create $PROJECT.zip ($RELEASEDATE)"
+git archive --format=tar --prefix=$PROJECT/ HEAD | (cd $TMP && tar xf -)
+rm $TMP/$PROJECT/.gitignore
+rm $TMP/$PROJECT/create_archive.sh
+rm -rf $TMP/$PROJECT/tests
+rm -rf $TMP/$PROJECT/archive
+### ----- Remove endnotesj.sty and related files (temporarily)
+rm $TMP/$PROJECT/endnotesj.sty
+rm -rf $TMP/$PROJECT/doc/endnotesj*.tex
+### -----
+perl -pi.bak -e "s/\\\$RELEASEDATE/$RELEASEDATE/g" $TMP/$PROJECT/README.md
+rm -f $TMP/$PROJECT/README.md.bak
+
+cd $TMP && zip -r $PWDF/$PROJECT.zip $PROJECT
+rm -rf $TMP/$PROJECT
+echo
+echo " * Done: $PROJECT.zip ($RELEASEDATE)"
